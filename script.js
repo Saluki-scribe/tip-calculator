6ff618bb4852c8a03d8132986c54e94e1555e770
Saluki-scribe <tara@nelsonwriting.com>
2017-12-07 14:49:29 -0500

    Configured tip display with conditional statement

--- script.js
+++ script.js
@@ -16,5 +16,25 @@ function calculateTip() {
     var serviceQuality = document.getElementById("serviceQuality").value;
     var numPeople = document.getElementById("totalPeople").value;
 
+    // Validation
+
+    if (billAmount === "" || serviceQuality == 0) {
+        window.alert("Please enter some values.");
+        return;
+    }
+
+    // Check to see if this input is empty or less than or equal to 1
+
+    if(numPeople === "" || numPeople <= 1) {
+        numPeople = 1;
+        window.alert("One person!")
+        document.getElementById("totalTip").style.display = "block";                
+        document.getElementById("each").style.display = "none";
+    } else {
+        window.alert("More than one person!")
+        document.getElementById("totalTip").style.display = "block";        
+        document.getElementById("each").style.display = "block";
+    }
+
     window.alert("Your amount is $" + billAmount + ", and your selected quality is " + serviceQuality + ". There are " + numPeople + " in your party.");
 }
