70f4309ba8468c8d67ac2f2f92703f1e7a62e1d8
Saluki-scribe <tara@nelsonwriting.com>
2017-12-07 14:28:08 -0500

    Added javascript to hide tip amount on load. Set up button to call tip calc function. Successfully tested.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-document.getElementById("tip").innerHTML = "$5.99"
+//Hide tip amount on load
+
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+// Click the button to call our custom function
+
+document.getElementById("calculate").onclick = function() { calculateTip(); };
+
+function calculateTip() {
+    window.alert("You called, madam?");
+}
